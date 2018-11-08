6e3d0ab18ea0e1c0fa6c938fb35307b2b0ef0a49
antonio9x9 <42574593+antonio9x9@users.noreply.github.com>
2018-11-08 08:42:55 -0200

    Add files via upload

---
+++ MDC_Recursivo.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+// Returna 1 cso não possuam MDC
+int MDC_rec(int x, int y){
+	int aux =0;
+	
+	// deixa o maior valor na variavel x
+	if(y>x){
+		aux = x;
+		x = y;
+		y =x;
+	}
+	
+	// caso o segundo temro seja 0, returna o 1º como o MDC
+	if(y == 0)
+		return x;
+	
+	// pega o resto da divisão de x por y
+	int r = x%y;
+	
+	// o caso base, é o resto da divisão dar zero
+	if(x%y == 0){
+		return y;
+	}
+	else{
+		// vai pegando o resto da divisão de x por y até que chegue no caso base.
+		x =y;
+		y = r;
+		r = x%y;
+		return MDC_rec(x, y);
+	}	
+}
+
+int main(){
+	
+	printf("MDC: %d\n", MDC_rec(5, 3));
+	
+	return 0;
+}
